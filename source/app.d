bd90fedfa0cc34bb8f6ef612c3fb9f99705eaef2
Szabo Bogdan <bogdan.szabo@altom.ro>
2016-04-14 16:40:19 +0300

    Added base main file

--- source/app.d
+++
@@ -1,3 +0,0 @@
-import std.stdio;
-
-import crate.request;
---
+++ source/main.d
@@ -0,0 +1,23 @@
+module crate.main;
+import std.getopt;
+
+void crateMain(string[] args, void delegate() startWebServer)
+{
+	string arduinoPort = "";
+	int webPort = 8080;
+
+	auto helpInformation = getopt(args,
+			"start", "Start web server", startWebServer,
+			"generate-openapi", "Generate OpenApi documentation", &generateOpenApi,
+			"webPort", "Web server port number", &webPort,
+			"arduinoPort", "Arduino port name", &arduinoPort);
+
+	if (helpInformation.helpWanted)
+	{
+		defaultGetoptPrinter("Some information about the program.", helpInformation.options);
+	}
+}
+
+void generateOpenApi() {
+  throw new Exception("not implemented");
+}
