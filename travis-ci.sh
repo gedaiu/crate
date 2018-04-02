8b031ae7c983e87e9c39b050d7d01108438f393a
Szabo Bogdan <szabobogdan@yahoo.com>
2018-04-03 00:58:21 +0300

    update travis script

--- travis-ci.sh
+++ travis-ci.sh
@@ -3,13 +3,13 @@ set -e -x -o pipefail
 
 # test for successful 32-bit build
 if [ "$DC" == "dmd" ]; then
-	dub build --arch=x86 --compiler=$DC
-	dub clean --all-packages
+	dub build --arch=x86 --compiler=$DC -v
+	dub clean --all-packages -v
 fi
 
 # test for successful release build
-dub build -b release --compiler=$DC
-dub clean --all-packages
+dub build -b release --compiler=$DC -v
+dub clean --all-packages -v
 
 # run unit tests
-dub test --compiler=$DC
+dub test --compiler=$DC -v
