c8f6b2fdc46ee5658a497e3a8b75b3256949b4ea
Szabo Bogdan <szabobogdan@yahoo.com>
2018-04-28 00:33:13 +0200

    add options request test

---
+++ tests/crate/http/router/options.d
@@ -0,0 +1,46 @@
+module tests.crate.http.router.options;
+
+import trial.interfaces;
+
+import trial.step;
+import trial.runner;
+import trial.discovery.spec;
+import fluent.asserts;
+import fluentasserts.vibe.request;
+
+
+import vibe.data.json;
+import vibe.inet.url;
+import vibe.http.router;
+
+import crate.http.router;
+import crate.policy.jsonapi;
+import crate.policy.restapi;
+
+import tests.crate.http.router.mocks;
+
+Site postSite(Site site) @safe {
+  site._id = "122";
+  return site;
+}
+
+alias s = Spec!({
+  describe("The crate router", {
+    describe("with a OPTIONS request", {
+      it("should get the access control headers", {
+        auto router = new URLRouter();
+        router.postWith!RestApi(&postSite);
+
+        Json dataUpdate = restSiteFixture.parseJsonString;
+
+        request(router)
+          .customMethod!(HTTPMethod.OPTIONS)(URL("http://localhost/sites"))
+            .send(dataUpdate)
+              .expectStatusCode(200)
+              .expectHeader("Access-Control-Allow-Origin", "*")
+              .expectHeader("Access-Control-Request-Method", "POST")
+              .end;
+      });
+    });
+  });
+});
